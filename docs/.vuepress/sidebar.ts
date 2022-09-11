1bcfd03dd2a62d263cb43d86af31b0c0b4ca9456
沧海红心 <canghaihongxin@163.com>
2022-09-11 20:27:03 +0800

    完善TBoot脚手架介绍

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,45 +2,54 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar([
   "/",
-  // "/home",
   {
-    text: "指南",
+    text: "TBoot脚手架",
     icon: "creative",
-    prefix: "/guide/",
-    link: "/guide/",
+    prefix: "/tboot/",
+    collapsable: true,
+    link: "/tboot/",
     children: "structure",
   },
-  {
-    text: "文章",
-    icon: "note",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "文章 1-4",
-        icon: "note",
-        collapsable: true,
-        prefix: "article/",
-        children: ["article1", "article2", "article3", "article4"],
-      },
-      {
-        text: "文章 5-12",
-        icon: "note",
-        children: [
-          {
-            text: "文章 5-8",
-            icon: "note",
-            collapsable: true,
-            prefix: "article/",
-            children: ["article5", "article6", "article7", "article8"],
-          },
-          {
-            text: "文章 9-12",
-            icon: "note",
-            children: ["article9", "article10", "article11", "article12"],
-          },
-        ],
-      },
-    ],
-  },
-  
+  // "/home",
+  // {
+  //   text: "指南",
+  //   icon: "creative",
+  //   prefix: "/guide/",
+  //   link: "/guide/",
+  //   children: "structure",
+  //   collapsable: true,
+  // },
+  // {
+  //   text: "文章",
+  //   icon: "note",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "文章 1-4",
+  //       icon: "note",
+  //       collapsable: true,
+  //       prefix: "article/",
+  //       children: ["article1", "article2", "article3", "article4"],
+  //     },
+  //     {
+  //       text: "文章 5-12",
+  //       icon: "note",
+  //       children: [
+  //         {
+  //           text: "文章 5-8",
+  //           icon: "note",
+  //           collapsable: true,
+  //           prefix: "article/",
+  //           children: ["article5", "article6", "article7", "article8"],
+  //         },
+  //         {
+  //           text: "文章 9-12",
+  //           icon: "note",
+  //           children: ["article9", "article10", "article11", "article12"],
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+
 ]);
---
+++ docs/tboot/deploy.md
@@ -0,0 +1,23 @@
+---
+title: 部署手册
+icon: markdown
+order: 1
+category: TBoot脚手架
+  - 
+---
+
+## 部署手册
+
+
+### 前端
+
+npm install 安装前端依赖
+npm run dev 启动项目
+
+
+### 后端
+
+先运行tboot.sql
+后端请使用tboot-back这个项目, application.yml 指定使用的配置文件,在配置文件中修改mysql redis 连接配置
+使用idea打开项目
+启动
