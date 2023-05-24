839b4bf920017917668fd0dbacc336bbcd327fea
canghaihongxin <30309837+canghaihongxin@users.noreply.github.com>
2023-05-24 10:14:17 +0000

    Deploying to gh-pages from @ canghaihongxin/weekly@80808eb6f1d773918f8223b9b29c54f8b54f53be 🚀

---
+++ assets/index.html-e641d424.js
@@ -0,0 +1,33 @@
+import{_ as t}from"./plugin-vue_export-helper-c27b6911.js";import{o as e,c as s,a as n}from"./app-2b4cfdb3.js";const o={};function i(a,l){return e(),s("div",null,[n(" ###  一起努力的小伙伴们 "),n(` 
+<div class="friends-content"> 
+
+<div class="friends-item">
+    <a href="https://www.hutool.cn" target="_blank">
+        <img :src="$withBase('https://plus.hutool.cn/images/hutool.svg')" class="no-zoom friends-item-img" alt="hutool">
+    </a>
+ 
+</div>
+
+
+<div class="friends-item">
+    <a href="https://shop108037867.taobao.com/" target="_blank">
+        <img :src="$withBase('/logo.svg')" class="no-zoom friends-item-img" alt="hutool">猿来衣舍
+    </a>
+</div>
+
+
+<div class="friends-item">
+    <a href="https://jpom.top/" target="_blank">
+        <img :src="$withBase('https://jpom.top/images/jpom_logo.png')" class="no-zoom friends-item-img" alt="hutool">
+    </a>
+</div>
+
+
+<div class="friends-item">
+    <a href="https://vampireachao.gitee.io/" target="_blank">
+        <img :src="$withBase('https://waibi.oss-cn-chengdu.aliyuncs.com/2020-06-01/head.jpg')" class="no-zoom friends-item-img" alt="hutool">
+    </a>
+</div>
+
+
+</div> `)])}const r=t(o,[["render",i],["__file","index.html.vue"]]);export{r as default};
--- assets/pageview-5f292e99.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./app-543f1512.js";const a=()=>e({serverURL:"https://weekly-waline-pvsifemc2-canghaihongxin.vercel.app/"});export{a as updatePageview};
